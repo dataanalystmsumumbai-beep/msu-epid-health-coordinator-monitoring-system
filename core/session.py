6d3ba282b4e36502d6c2754739bd779ffd990026
dataanalystmsumumbai-beep <dataanalystmsumumbai@gmail.com>
2026-07-28 15:44:20 +0530

    Update session.py

--- core/session.py
+++ core/session.py
@@ -1,38 +1,40 @@
 import streamlit as st
 
 
-SESSION_DEFAULTS = {
-
+DEFAULT_SESSION = {
     "logged_in": False,
-
-    "username": "",
-
-    "name": "",
-
-    "role": "",
-
     "user_id": "",
-
+    "username": "",
+    "name": "",
+    "role": "",
     "ward": "",
-
-    "login_time": "",
-
+    "login_time": ""
 }
 
 
 def initialize_session():
 
-    for key, value in SESSION_DEFAULTS.items():
+    for key, value in DEFAULT_SESSION.items():
 
         if key not in st.session_state:
 
             st.session_state[key] = value
 
 
+def login(user):
+
+    st.session_state.logged_in = True
+    st.session_state.user_id = user["User_ID"]
+    st.session_state.username = user["Username"]
+    st.session_state.name = user["Name"]
+    st.session_state.role = user["Role"]
+    st.session_state.ward = user["Ward"]
+
+
 def logout():
 
-    for key in SESSION_DEFAULTS.keys():
+    for key, value in DEFAULT_SESSION.items():
 
-        st.session_state[key] = SESSION_DEFAULTS[key]
+        st.session_state[key] = value
 
     st.rerun()
