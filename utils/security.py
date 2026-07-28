94f443196bc1f75de6a9f437c5c9a2feb99fd1b3
dataanalystmsumumbai-beep <dataanalystmsumumbai@gmail.com>
2026-07-28 15:44:47 +0530

    Update security.py

--- utils/security.py
+++ utils/security.py
@@ -1,13 +1,19 @@
 import re
 
+from config.config import (
+    PASSWORD_MIN_LENGTH,
+    PASSWORD_MAX_LENGTH,
+    ALLOWED_SPECIAL_CHARACTERS
+)
 
-ALLOWED_SPECIAL_CHARACTERS = "@#$-._"
 
+def validate_password(password):
 
-def validate_password(password: str):
+    if len(password) < PASSWORD_MIN_LENGTH:
+        return False, f"Minimum {PASSWORD_MIN_LENGTH} characters required."
 
-    if len(password) < 8 or len(password) > 10:
-        return False, "Password must be between 8 and 10 characters."
+    if len(password) > PASSWORD_MAX_LENGTH:
+        return False, f"Maximum {PASSWORD_MAX_LENGTH} characters allowed."
 
     if not re.search(r"[A-Za-z]", password):
         return False, "Password must contain at least one alphabet."
@@ -25,4 +31,4 @@ def validate_password(password: str):
     if re.search(invalid, password):
         return False, "Invalid special character used."
 
-    return True, "Valid Password"
+    return True, "Valid"
