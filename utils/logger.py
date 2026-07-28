b4b39ed6ad3fffe996c196d7a8d219d631ed1d9f
dataanalystmsumumbai-beep <dataanalystmsumumbai@gmail.com>
2026-07-28 15:27:57 +0530

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,1 +1,30 @@
+from utils.google_sheet import append_row
+from utils.datetime_utils import current_datetime
 
+
+def save_audit_log(
+    user,
+    role,
+    action,
+    details
+):
+
+    append_row(
+
+        "05_Audit_Log",
+
+        [
+
+            current_datetime(),
+
+            user,
+
+            role,
+
+            action,
+
+            details
+
+        ]
+
+    )
