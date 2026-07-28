d773316511861b48b5850e96281498de3e21bbde
dataanalystmsumumbai-beep <dataanalystmsumumbai@gmail.com>
2026-07-28 13:22:18 +0530

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-APP_NAME = "MSU/EPID Health Coordinator Monitoring System"
-
-VERSION = "3.0 Enterprise"
-
-DEPARTMENT = "Metropolitan Surveillance Unit (MSU) | Epidemiology Cell (EPID), Mumbai"
-
-GOOGLE_SHEET_URL = ""
