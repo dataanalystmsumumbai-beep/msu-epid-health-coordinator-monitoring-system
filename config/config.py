a953ac0380901633c2150e1bd88436b2a74e5d76
dataanalystmsumumbai-beep <dataanalystmsumumbai@gmail.com>
2026-07-28 15:24:14 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,9 +1,9 @@
 import streamlit as st
 from google.oauth2.service_account import Credentials
 
-# -------------------------------
+# ------------------------------
 # Google Sheet Configuration
-# -------------------------------
+# ------------------------------
 
 SCOPES = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -12,8 +12,28 @@ SCOPES = [
 
 SHEET_NAME = "MSU HCMS Database"
 
+
 def get_google_credentials():
     return Credentials.from_service_account_info(
         st.secrets["gcp_service_account"],
         scopes=SCOPES
     )
+
+
+# ------------------------------
+# Application Configuration
+# ------------------------------
+
+APP_NAME = "MSU EPID Health Coordinator Monitoring System"
+
+APP_VERSION = "3.0.0"
+
+DEVELOPER_USERNAME = "developer"
+
+MAX_COORDINATOR_EDIT = 6
+
+PASSWORD_MIN_LENGTH = 8
+
+PASSWORD_MAX_LENGTH = 10
+
+ALLOWED_SPECIAL_CHARACTERS = "@#$-._"
