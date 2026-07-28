e08381340cd6753f9a1713b23ca8aaed6f70547a
dataanalystmsumumbai-beep <dataanalystmsumumbai@gmail.com>
2026-07-28 15:25:35 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -2,7 +2,11 @@ import gspread
 
 from config.config import get_google_credentials
 
+
 def connect_database():
+    """
+    Connect to Google Spreadsheet
+    """
 
     credentials = get_google_credentials()
 
