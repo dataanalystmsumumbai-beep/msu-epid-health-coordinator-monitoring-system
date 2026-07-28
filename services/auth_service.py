bd27f8535e705c4459c9a496562237fb72f30794
dataanalystmsumumbai-beep <dataanalystmsumumbai@gmail.com>
2026-07-28 15:21:41 +0530

    Update auth_service.py

--- services/auth_service.py
+++ services/auth_service.py
@@ -1,1 +1,34 @@
+class AuthService:
 
+    def login(
+        self,
+        username,
+        password
+    ):
+
+        pass
+
+
+    def logout(self):
+
+        pass
+
+
+    def validate_user(self):
+
+        pass
+
+
+    def developer_login(self):
+
+        pass
+
+
+    def admin_login(self):
+
+        pass
+
+
+    def coordinator_login(self):
+
+        pass
