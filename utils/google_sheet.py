d3e6860dae95889986e9d9cd4b3a0ed526915169
dataanalystmsumumbai-beep <dataanalystmsumumbai@gmail.com>
2026-07-28 14:45:18 +0530

    Update google_sheet.py

--- utils/google_sheet.py
+++ utils/google_sheet.py
@@ -1,4 +1,37 @@
-import pandas as pd
+import gspread
+from database.database import connect_database
 
-def test_connection():
-    return "Database Module Ready"
+
+def get_spreadsheet(sheet_name: str):
+    """
+    Connect to Google Spreadsheet.
+    Create Spreadsheet if it does not exist.
+    """
+
+    client = connect_database()
+
+    try:
+        spreadsheet = client.open(sheet_name)
+
+    except gspread.SpreadsheetNotFound:
+        spreadsheet = client.create(sheet_name)
+
+    return spreadsheet
+
+
+def get_or_create_worksheet(spreadsheet, worksheet_name, headers):
+
+    try:
+        worksheet = spreadsheet.worksheet(worksheet_name)
+
+    except gspread.WorksheetNotFound:
+
+        worksheet = spreadsheet.add_worksheet(
+            title=worksheet_name,
+            rows=1000,
+            cols=max(len(headers), 20)
+        )
+
+        worksheet.append_row(headers)
+
+    return worksheet
