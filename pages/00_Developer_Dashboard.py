73577e35131065cddb29f283ce3e4df3a7ed0f58
dataanalystmsumumbai-beep <dataanalystmsumumbai@gmail.com>
2026-07-29 14:47:22 +0530

    Update 00_Developer_Dashboard.py

--- pages/00_Developer_Dashboard.py
+++ pages/00_Developer_Dashboard.py
@@ -1,1 +1,37 @@
+import streamlit as st
 
+from core.session import logout
+
+if (
+    "logged_in" not in st.session_state
+    or
+    not st.session_state.logged_in
+):
+
+    st.switch_page("app.py")
+
+if st.session_state.role != "Developer":
+
+    st.error("Access Denied")
+
+    st.stop()
+
+st.title("👨‍💻 Developer Dashboard")
+
+st.write(
+    f"Welcome : {st.session_state.full_name}"
+)
+
+st.write(
+    f"Username : {st.session_state.username}"
+)
+
+st.write(
+    f"Role : {st.session_state.role}"
+)
+
+st.divider()
+
+if st.button("Logout"):
+
+    logout()
