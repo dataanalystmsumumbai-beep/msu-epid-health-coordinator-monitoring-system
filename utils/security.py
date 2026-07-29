1aa91d68fc284db220911df33f00c5c33c8d08a9
dataanalystmsumumbai-beep <dataanalystmsumumbai@gmail.com>
2026-07-29 13:08:26 +0530

    Update security.py

--- utils/security.py
+++ utils/security.py
@@ -1,34 +1,11 @@
-import re
-
-from config.config import (
-    PASSWORD_MIN_LENGTH,
-    PASSWORD_MAX_LENGTH,
-    ALLOWED_SPECIAL_CHARACTERS
-)
+import hashlib
 
 
-def validate_password(password):
+def hash_password(password: str) -> str:
 
-    if len(password) < PASSWORD_MIN_LENGTH:
-        return False, f"Minimum {PASSWORD_MIN_LENGTH} characters required."
+    return hashlib.sha256(password.encode()).hexdigest()
 
-    if len(password) > PASSWORD_MAX_LENGTH:
-        return False, f"Maximum {PASSWORD_MAX_LENGTH} characters allowed."
 
-    if not re.search(r"[A-Za-z]", password):
-        return False, "Password must contain at least one alphabet."
+def verify_password(password: str, password_hash: str) -> bool:
 
-    if not re.search(r"[0-9]", password):
-        return False, "Password must contain at least one number."
-
-    special = "[" + re.escape(ALLOWED_SPECIAL_CHARACTERS) + "]"
-
-    if not re.search(special, password):
-        return False, "Password must contain at least one special character."
-
-    invalid = "[^A-Za-z0-9" + re.escape(ALLOWED_SPECIAL_CHARACTERS) + "]"
-
-    if re.search(invalid, password):
-        return False, "Invalid special character used."
-
-    return True, "Valid"
+    return hash_password(password) == password_hash
