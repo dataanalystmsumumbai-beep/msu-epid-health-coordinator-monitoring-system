981829ec129c4d362409bdd3e320b4512d18342d
dataanalystmsumumbai-beep <dataanalystmsumumbai@gmail.com>
2026-07-29 15:54:40 +0530

    Update 01_Admin_Dashboard.py

--- pages/01_Admin_Dashboard.py
+++ pages/01_Admin_Dashboard.py
@@ -1,3 +1,35 @@
 import streamlit as st
 
-st.title("Under Development")
+from core.session import logout
+
+if (
+    "logged_in" not in st.session_state
+    or
+    not st.session_state.logged_in
+):
+
+    st.switch_page("app.py")
+
+if st.session_state.role not in [
+
+    "Developer",
+
+    "Admin"
+
+]:
+
+    st.error("Access Denied")
+
+    st.stop()
+
+st.title("👨‍💼 Admin Dashboard")
+
+st.write(
+
+    f"Welcome {st.session_state.full_name}"
+
+)
+
+if st.button("Logout"):
+
+    logout()
