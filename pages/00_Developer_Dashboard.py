c9f658761425469540095db3176f91eaaa075a38
dataanalystmsumumbai-beep <dataanalystmsumumbai@gmail.com>
2026-08-01 13:10:58 +0530

    Update 00_Developer_Dashboard.py

--- pages/00_Developer_Dashboard.py
+++ pages/00_Developer_Dashboard.py
@@ -39,7 +39,7 @@ if (
 # Load Users
 # ------------------------------------------------
 
-users = UserService.get_all_users()
+users = []
 
 # ------------------------------------------------
 # Sidebar
@@ -99,6 +99,7 @@ st.json(dict(st.session_state))
 # Dashboard Cards
 # ------------------------------------------------
 
+st.success("Dashboard Loaded Successfully")
 total_users = len(users)
 
 developer_count = len(
